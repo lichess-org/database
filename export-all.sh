84da86140f2e9e83c7b92462db98b681b3a6822c
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-06-05 10:56:41 +0200

    also export 2013 games

--- export-all.sh
+++ export-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for year in 2014 2015 2016 2017; do
+for year in 2013 2014 2015 2016 2017; do
   for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
     date="$year-$month"
     echo "---------------------------"
