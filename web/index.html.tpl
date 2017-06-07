69e49c220c02d546b0abf0c5078ca890cb0a5907
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-06-07 11:04:04 +0200

    add static index generator

---
+++ web/index.html.tpl
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <link href='https://fonts.googleapis.com/css?family=Chivo:900' rel='stylesheet' type='text/css'>
+    <style><!-- style --></style>
+    <title>lichess.org game database</title>
+  </head>
+
+  <body>
+    <div id="container">
+      <div class="inner">
+
+        <header>
+          <h2>lichess.org game database</h2>
+        </header>
+        <hr>
+        <section id="main_content">
+          <p>All standard rated games, in PGN, with stockfish evaluations and move times included.</p>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Size</th>
+                <th>Download</th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- files -->
+            </tbody>
+          </table>
+
+        </section>
+
+        <footer>
+          All games played on <a href="https://lichess.org">lichess.org</a> are in the public domain. Enjoy!
+        </footer>
+
+      </div>
+    </div>
+
+
+  </body>
+</html>
---
+++ web/index.js
@@ -0,0 +1,55 @@
+const fs = require('fs-extra');
+const prettyBytes = require('pretty-bytes');
+const moment = require('moment');
+
+const sourceDir = process.argv[2];
+const indexFile = 'index.html';
+const indexTpl = indexFile + '.tpl';
+const styleFile = 'style.css';
+
+function fileInfo(n) {
+  const path = sourceDir + '/' + n;
+  return fs.stat(path).then(s => {
+    const dateStr = n.replace(/.+(\d{4}-\d{2})\.pgn\.bz2/, '$1');
+    const m = moment(dateStr);
+    const shortName = n.replace(/.+(\d{4}-\d{2}.+)$/, '$1');
+    return {
+      name: n,
+      shortName: shortName,
+      path: path,
+      size: prettyBytes(s.size),
+      date: m
+    };
+  });
+}
+
+function getFiles() {
+  return fs.readdir(sourceDir).then(items => {
+    return Promise.all(items.filter(n => n.includes('.pgn.bz2')).map(fileInfo));
+  });
+}
+
+function getIndex() {
+  return fs.readFile(indexFile, { encoding: 'utf8' });
+}
+
+function renderTable(files) {
+  return files.map(f => {
+    return `<tr>
+    <td>${f.date.format('MMMM YYYY')}</td>
+    <td>${f.size}</td>
+    <td><a href="${f.name}">${f.shortName}</a></td>
+    </tr>`;
+  }).join('\n');
+}
+
+Promise.all([
+  getFiles(),
+  fs.readFile(indexTpl, { encoding: 'utf8' }),
+  fs.readFile(styleFile, { encoding: 'utf8' })
+]).then(([files, tpl, style]) => {
+  const rendered = tpl
+    .replace(/<!-- files -->/, renderTable(files))
+    .replace(/<!-- style -->/, style);
+  fs.writeFile(indexFile, rendered);
+});
