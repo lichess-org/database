d74c0bc890970a7aa449a4f61de3f80836b91978
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-06-07 12:28:08 +0200

    fix game nb estimates

--- web/index.js
+++ web/index.js
@@ -9,7 +9,7 @@ const styleFile = 'style.css';
 
 const clockSince = moment('2017-04');
 
-const sizePerGameBeforeClock = 124;
+const sizePerGameBeforeClock = 260;
 const sizePerGameAfterClock = 2400;
 
 function numberFormat(n) {
@@ -65,7 +65,7 @@ function renderTotal(files) {
   return `<tr>
   <td>Total: ${files.length} files</td>
   <td>${prettyBytes(files.map(f => f.size).reduce((a, b) => a + b))}</td>
-  <td>${files.map(f => f.games).reduce((a, b) => a + b)}</td>
+  <td>${numberFormat(files.map(f => f.games).reduce((a, b) => a + b))}</td>
   <td></td>
   <td></td>
   <td></td>
