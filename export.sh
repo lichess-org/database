c19ee17cec1439be4d1349fd03aec21db70d99dd
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-06-08 10:40:38 +0200

    remove old bz2 file

--- export.sh
+++ export.sh
@@ -3,6 +3,7 @@
 month=${1}
 dir=${2}
 file="lichess_db_standard_rated_$month.pgn"
+bz2file="$file.bz2"
 
 echo "Export $month to $file"
 
@@ -13,17 +14,18 @@ cd "$dir"
 touch counts.txt
 grep -v -F "$file" counts.txt > counts.txt.new || touch counts.txt.new
 games=$(grep --count -F '[Site ' "$file")
-echo "$file.bz2 $games" >> counts.txt.new
+echo "$bz2file $games" >> counts.txt.new
 mv counts.txt.new counts.txt
 
-echo "Compressing $games games to $file.bz2"
+echo "Compressing $games games to $bz2file"
 
+rm -f $bz2file
 bzip2 $file
 
-echo "Check summing $file.bz2"
+echo "Check summing $bz2file"
 touch sha256sums.txt
-grep -v -F "$file.bz2" sha256sums.txt > sha256sums.txt.new || touch sha256sums.txt.new
-sha256sum "$file.bz2" | tee --append sha256sums.txt.new
+grep -v -F "$bz2file" sha256sums.txt > sha256sums.txt.new || touch sha256sums.txt.new
+sha256sum "$bz2file" | tee --append sha256sums.txt.new
 mv sha256sums.txt.new sha256sums.txt
 
 cd -
