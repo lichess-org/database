4d2511c070ad2e114bd7ea2db5e6031c83b4d8d5
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-06-08 10:35:50 +0200

    checksum bz2 files

--- export.sh
+++ export.sh
@@ -10,12 +10,6 @@ sbt "run $month $dir/$file"
 
 cd "$dir"
 
-echo "Check summing $file.bz2"
-touch sha256sums.txt
-grep -v -F "$file" sha256sums.txt > sha256sums.txt.new || touch sha256sums.txt.new
-sha256sum "$file" | tee --append sha256sums.txt.new
-mv sha256sums.txt.new sha256sums.txt
-
 touch counts.txt
 grep -v -F "$file" counts.txt > counts.txt.new || touch counts.txt.new
 games=$(grep --count -F '[Site ' "$file")
@@ -25,6 +19,12 @@ mv counts.txt.new counts.txt
 echo "Compressing $games games to $file.bz2"
 
 bzip2 $file
+
+echo "Check summing $file.bz2"
+touch sha256sums.txt
+grep -v -F "$file.bz2" sha256sums.txt > sha256sums.txt.new || touch sha256sums.txt.new
+sha256sum "$file.bz2" | tee --append sha256sums.txt.new
+mv sha256sums.txt.new sha256sums.txt
 
 cd -
 echo "Done!"
