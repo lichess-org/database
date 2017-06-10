817b78868608f93183e6a572f1a5ad4efbea89ac
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-06-10 13:10:10 +0200

    tweak export-all script

--- export-all.sh
+++ export-all.sh
@@ -2,12 +2,27 @@
 
 dir=${1}
 
+export() {
+    echo "---------------------------"
+    ./export.sh "$1-$2" $dir
+}
+
 echo "Export all to $dir"
 
-for year in 2013 2014 2015 2016 2017; do
+for year in 2013; do
+  for month in 08 09 10 11 12; do
+    export $year $month
+  done
+done
+
+for year in 2014 2015 2016; do
   for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
-    date="$year-$month"
-    echo "---------------------------"
-    ./export.sh $date $dir
+    export $year $month
+  done
+done
+
+for year in 2017; do
+  for month in 01 02 03 04 05; do
+    export $year $month
   done
 done
