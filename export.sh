f74675730b6f6a0db7d4d62d8492e7ff193a8dc8
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-10-30 10:58:25 -0500

    export is a sh keyword

--- export.sh
+++ export.sh
@@ -3,7 +3,7 @@
 month=${1}
 dir=${2}
 
-export() {
+export_variant() {
     echo "---------------------------"
     mkdir -p $dir/$1
     ./export-single-variant.sh $month $dir/$1 $1
@@ -12,7 +12,7 @@ export() {
 variants="standard chess960 antichess atomic crazyhouse horde kingOfTheHill racingKings threeCheck"
 
 for variant in $variants; do
-  export $variant
+  export_variant $variant
 done
 
 cd -
