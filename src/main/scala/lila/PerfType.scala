ed9cf380cf876cf8f18c6985b009ed0288fe5b78
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-02-01 18:21:20 -0600

    upgrade all deps WIP

--- src/main/scala/lila/PerfType.scala
+++ src/main/scala/lila/PerfType.scala
@@ -17,137 +17,174 @@ sealed abstract class PerfType(
 
 object PerfType {
 
-  case object UltraBullet extends PerfType(
-    0,
-    key = "ultraBullet",
-    name = Speed.UltraBullet.name,
-    title = Speed.UltraBullet.title,
-    iconChar = '{'
-  )
+  case object UltraBullet
+      extends PerfType(
+        0,
+        key = "ultraBullet",
+        name = Speed.UltraBullet.name,
+        title = Speed.UltraBullet.title,
+        iconChar = '{'
+      )
 
-  case object Bullet extends PerfType(
-    1,
-    key = "bullet",
-    name = Speed.Bullet.name,
-    title = Speed.Bullet.title,
-    iconChar = 'T'
-  )
+  case object Bullet
+      extends PerfType(
+        1,
+        key = "bullet",
+        name = Speed.Bullet.name,
+        title = Speed.Bullet.title,
+        iconChar = 'T'
+      )
 
-  case object Blitz extends PerfType(
-    2,
-    key = "blitz",
-    name = Speed.Blitz.name,
-    title = Speed.Blitz.title,
-    iconChar = ')'
-  )
+  case object Blitz
+      extends PerfType(
+        2,
+        key = "blitz",
+        name = Speed.Blitz.name,
+        title = Speed.Blitz.title,
+        iconChar = ')'
+      )
 
-  case object Rapid extends PerfType(
-    6,
-    key = "rapid",
-    name = Speed.Rapid.name,
-    title = Speed.Rapid.title,
-    iconChar = '#'
-  )
+  case object Rapid
+      extends PerfType(
+        6,
+        key = "rapid",
+        name = Speed.Rapid.name,
+        title = Speed.Rapid.title,
+        iconChar = '#'
+      )
 
-  case object Classical extends PerfType(
-    3,
-    key = "classical",
-    name = Speed.Classical.name,
-    title = Speed.Classical.title,
-    iconChar = '+'
-  )
+  case object Classical
+      extends PerfType(
+        3,
+        key = "classical",
+        name = Speed.Classical.name,
+        title = Speed.Classical.title,
+        iconChar = '+'
+      )
 
-  case object Correspondence extends PerfType(
-    4,
-    key = "correspondence",
-    name = "Correspondence",
-    title = "Correspondence (days per turn)",
-    iconChar = ';'
-  )
+  case object Correspondence
+      extends PerfType(
+        4,
+        key = "correspondence",
+        name = "Correspondence",
+        title = "Correspondence (days per turn)",
+        iconChar = ';'
+      )
 
-  case object Standard extends PerfType(
-    5,
-    key = "standard",
-    name = chess.variant.Standard.name,
-    title = "Standard rules of chess",
-    iconChar = '8'
-  )
+  case object Standard
+      extends PerfType(
+        5,
+        key = "standard",
+        name = chess.variant.Standard.name,
+        title = "Standard rules of chess",
+        iconChar = '8'
+      )
 
-  case object Chess960 extends PerfType(
-    11,
-    key = "chess960",
-    name = chess.variant.Chess960.name,
-    title = "Chess960 variant",
-    iconChar = '''
-  )
+  case object Chess960
+      extends PerfType(
+        11,
+        key = "chess960",
+        name = chess.variant.Chess960.name,
+        title = "Chess960 variant",
+        iconChar = '\''
+      )
 
-  case object KingOfTheHill extends PerfType(
-    12,
-    key = "kingOfTheHill",
-    name = chess.variant.KingOfTheHill.name,
-    title = "King of the Hill variant",
-    iconChar = '('
-  )
+  case object KingOfTheHill
+      extends PerfType(
+        12,
+        key = "kingOfTheHill",
+        name = chess.variant.KingOfTheHill.name,
+        title = "King of the Hill variant",
+        iconChar = '('
+      )
 
-  case object Antichess extends PerfType(
-    13,
-    key = "antichess",
-    name = chess.variant.Antichess.name,
-    title = "Antichess variant",
-    iconChar = '@'
-  )
+  case object Antichess
+      extends PerfType(
+        13,
+        key = "antichess",
+        name = chess.variant.Antichess.name,
+        title = "Antichess variant",
+        iconChar = '@'
+      )
 
-  case object Atomic extends PerfType(
-    14,
-    key = "atomic",
-    name = chess.variant.Atomic.name,
-    title = "Atomic variant",
-    iconChar = '>'
-  )
+  case object Atomic
+      extends PerfType(
+        14,
+        key = "atomic",
+        name = chess.variant.Atomic.name,
+        title = "Atomic variant",
+        iconChar = '>'
+      )
 
-  case object ThreeCheck extends PerfType(
-    15,
-    key = "threeCheck",
-    name = chess.variant.ThreeCheck.name,
-    title = "Three-check variant",
-    iconChar = '.'
-  )
+  case object ThreeCheck
+      extends PerfType(
+        15,
+        key = "threeCheck",
+        name = chess.variant.ThreeCheck.name,
+        title = "Three-check variant",
+        iconChar = '.'
+      )
 
-  case object Horde extends PerfType(
-    16,
-    key = "horde",
-    name = chess.variant.Horde.name,
-    title = "Horde variant",
-    iconChar = '_'
-  )
+  case object Horde
+      extends PerfType(
+        16,
+        key = "horde",
+        name = chess.variant.Horde.name,
+        title = "Horde variant",
+        iconChar = '_'
+      )
 
-  case object RacingKings extends PerfType(
-    17,
-    key = "racingKings",
-    name = chess.variant.RacingKings.name,
-    title = "Racing kings variant",
-    iconChar = ''
-  )
+  case object RacingKings
+      extends PerfType(
+        17,
+        key = "racingKings",
+        name = chess.variant.RacingKings.name,
+        title = "Racing kings variant",
+        iconChar = ''
+      )
 
-  case object Crazyhouse extends PerfType(
-    18,
-    key = "crazyhouse",
-    name = chess.variant.Crazyhouse.name,
-    title = "Crazyhouse variant",
-    iconChar = ''
-  )
+  case object Crazyhouse
+      extends PerfType(
+        18,
+        key = "crazyhouse",
+        name = chess.variant.Crazyhouse.name,
+        title = "Crazyhouse variant",
+        iconChar = ''
+      )
 
-  case object Puzzle extends PerfType(
-    20,
-    key = "puzzle",
-    name = "Training",
-    title = "Training puzzles",
-    iconChar = '-'
-  )
+  case object Puzzle
+      extends PerfType(
+        20,
+        key = "puzzle",
+        name = "Training",
+        title = "Training puzzles",
+        iconChar = '-'
+      )
 
-  val all: List[PerfType] = List(UltraBullet, Bullet, Blitz, Rapid, Classical, Correspondence, Standard, Crazyhouse, Chess960, KingOfTheHill, ThreeCheck, Antichess, Atomic, Horde, RacingKings, Puzzle)
-  val byKey = all map { p => (p.key, p) } toMap
-  val byId = all map { p => (p.id, p) } toMap
+  val all: List[PerfType] = List(
+    UltraBullet,
+    Bullet,
+    Blitz,
+    Rapid,
+    Classical,
+    Correspondence,
+    Standard,
+    Crazyhouse,
+    Chess960,
+    KingOfTheHill,
+    ThreeCheck,
+    Antichess,
+    Atomic,
+    Horde,
+    RacingKings,
+    Puzzle
+  )
+  val byKey = all map { p =>
+    (p.key, p)
+  } toMap
+  val byId = all map { p =>
+    (p.id, p)
+  } toMap
 
   val default = Standard
 
@@ -160,11 +197,50 @@ object PerfType {
 
   def id2key(id: Int): Option[String] = byId get id map (_.key)
 
-  val nonPuzzle: List[PerfType] = List(UltraBullet, Bullet, Blitz, Rapid, Classical, Correspondence, Crazyhouse, Chess960, KingOfTheHill, ThreeCheck, Antichess, Atomic, Horde, RacingKings)
+  val nonPuzzle: List[PerfType] = List(
+    UltraBullet,
+    Bullet,
+    Blitz,
+    Rapid,
+    Classical,
+    Correspondence,
+    Crazyhouse,
+    Chess960,
+    KingOfTheHill,
+    ThreeCheck,
+    Antichess,
+    Atomic,
+    Horde,
+    RacingKings
+  )
   val nonGame: List[PerfType] = List(Puzzle)
-  val leaderboardable: List[PerfType] = List(Bullet, Blitz, Rapid, Classical, UltraBullet, Crazyhouse, Chess960, KingOfTheHill, ThreeCheck, Antichess, Atomic, Horde, RacingKings)
-  val variants: List[PerfType] = List(Crazyhouse, Chess960, KingOfTheHill, ThreeCheck, Antichess, Atomic, Horde, RacingKings)
-  val standard: List[PerfType] = List(Bullet, Blitz, Rapid, Classical, Correspondence)
+  val leaderboardable: List[PerfType] = List(
+    Bullet,
+    Blitz,
+    Rapid,
+    Classical,
+    UltraBullet,
+    Crazyhouse,
+    Chess960,
+    KingOfTheHill,
+    ThreeCheck,
+    Antichess,
+    Atomic,
+    Horde,
+    RacingKings
+  )
+  val variants: List[PerfType] = List(
+    Crazyhouse,
+    Chess960,
+    KingOfTheHill,
+    ThreeCheck,
+    Antichess,
+    Atomic,
+    Horde,
+    RacingKings
+  )
+  val standard: List[PerfType] =
+    List(Bullet, Blitz, Rapid, Classical, Correspondence)
 
   def isGame(pt: PerfType) = !nonGame.contains(pt)
 }
--- src/main/scala/lila/game/BSONHandlers.scala
+++ src/main/scala/lila/game/BSONHandlers.scala
@@ -2,10 +2,20 @@ package lila.game
 
 import org.joda.time.DateTime
 import reactivemongo.bson._
-import scala.collection.breakOut
 
-import chess.variant.{ Variant, Crazyhouse }
-import chess.{ CheckCount, Color, Clock, White, Black, Status, Mode, UnmovedRooks, History => ChessHistory, Game => ChessGame }
+import chess.variant.{Variant, Crazyhouse}
+import chess.{
+  CheckCount,
+  Color,
+  Clock,
+  White,
+  Black,
+  Status,
+  Mode,
+  UnmovedRooks,
+  History => ChessHistory,
+  Game => ChessGame
+}
 
 import lila.db.BSON
 import lila.db.dsl._
@@ -14,47 +24,52 @@ object BSONHandlers {
 
   import lila.db.ByteArray.ByteArrayBSONHandler
 
-  private[game] implicit val checkCountWriter = new BSONWriter[CheckCount, BSONArray] {
-    def write(cc: CheckCount) = BSONArray(cc.white, cc.black)
-  }
+  private[game] implicit val checkCountWriter =
+    new BSONWriter[CheckCount, BSONArray] {
+      def write(cc: CheckCount) = BSONArray(cc.white, cc.black)
+    }
 
   implicit val StatusBSONHandler = new BSONHandler[BSONInteger, Status] {
-    def read(bsonInt: BSONInteger): Status = Status(bsonInt.value) err s"No such status: ${bsonInt.value}"
+    def read(bsonInt: BSONInteger): Status =
+      Status(bsonInt.value) err s"No such status: ${bsonInt.value}"
     def write(x: Status) = BSONInteger(x.id)
   }
 
-  private[game] implicit val unmovedRooksHandler = new BSONHandler[BSONBinary, UnmovedRooks] {
-    def read(bin: BSONBinary): UnmovedRooks = BinaryFormat.unmovedRooks.read {
-      ByteArrayBSONHandler.read(bin)
+  private[game] implicit val unmovedRooksHandler =
+    new BSONHandler[BSONBinary, UnmovedRooks] {
+      def read(bin: BSONBinary): UnmovedRooks = BinaryFormat.unmovedRooks.read {
+        ByteArrayBSONHandler.read(bin)
+      }
+      def write(x: UnmovedRooks): BSONBinary = ByteArrayBSONHandler.write {
+        BinaryFormat.unmovedRooks.write(x)
+      }
     }
-    def write(x: UnmovedRooks): BSONBinary = ByteArrayBSONHandler.write {
-      BinaryFormat.unmovedRooks.write(x)
+
+  private[game] implicit val crazyhouseDataBSONHandler =
+    new BSON[Crazyhouse.Data] {
+
+      import Crazyhouse._
+
+      def reads(r: BSON.Reader) = Crazyhouse.Data(
+        pockets = {
+          val (white, black) = {
+            r.str("p")
+              .flatMap(chess.Piece.fromChar)(breakOut): List[chess.Piece]
+          }.partition(_ is chess.White)
+          Pockets(
+            white = Pocket(white.map(_.role)),
+            black = Pocket(black.map(_.role))
+          )
+        },
+        promoted = r.str("t").flatMap(chess.Pos.piotr)(breakOut)
+      )
+
+      def writes(w: BSON.Writer, o: Crazyhouse.Data) = ???
     }
-  }
-
-  private[game] implicit val crazyhouseDataBSONHandler = new BSON[Crazyhouse.Data] {
-
-    import Crazyhouse._
-
-    def reads(r: BSON.Reader) = Crazyhouse.Data(
-      pockets = {
-        val (white, black) = {
-          r.str("p").flatMap(chess.Piece.fromChar)(breakOut): List[chess.Piece]
-        }.partition(_ is chess.White)
-        Pockets(
-          white = Pocket(white.map(_.role)),
-          black = Pocket(black.map(_.role))
-        )
-      },
-      promoted = r.str("t").flatMap(chess.Pos.piotr)(breakOut)
-    )
-
-    def writes(w: BSON.Writer, o: Crazyhouse.Data) = ???
-  }
 
   implicit val gameBSONHandler: BSON[Game] = new BSON[Game] {
 
-    import Game.{ BSONFields => F }
+    import Game.{BSONFields => F}
     import Player.playerBSONHandler
 
     private val emptyPlayerBuilder = playerBSONHandler.read(BSONDocument())
@@ -65,13 +80,17 @@ object BSONHandlers {
       val plies = r int F.turns
       val turnColor = Color.fromPly(plies)
 
-      val decoded = r.bytesO(F.huffmanPgn).map { PgnStorage.Huffman.decode(_, plies) } getOrElse {
+      val decoded = r.bytesO(F.huffmanPgn).map {
+        PgnStorage.Huffman.decode(_, plies)
+      } getOrElse {
         val clm = r.get[CastleLastMove](F.castleLastMove)
         PgnStorage.Decoded(
           pgnMoves = PgnStorage.OldBin.decode(r bytesD F.oldPgn, plies),
           pieces = BinaryFormat.piece.read(r bytes F.binaryPieces, gameVariant),
-          positionHashes = r.getO[chess.PositionHash](F.positionHashes) getOrElse Array.empty,
-          unmovedRooks = r.getO[UnmovedRooks](F.unmovedRooks) getOrElse UnmovedRooks.default,
+          positionHashes = r
+            .getO[chess.PositionHash](F.positionHashes) getOrElse Array.empty,
+          unmovedRooks = r
+            .getO[UnmovedRooks](F.unmovedRooks) getOrElse UnmovedRooks.default,
           lastMove = clm.lastMove,
           castles = clm.castles,
           format = PgnStorage.OldBin
@@ -80,8 +99,14 @@ object BSONHandlers {
 
       val winC = r boolO F.winnerColor map Color.apply
       val uids = r.getO[List[String]](F.playerUids) getOrElse Nil
-      val (whiteUid, blackUid) = (uids.headOption.filter(_.nonEmpty), uids.lift(1).filter(_.nonEmpty))
-      def makePlayer(field: String, color: Color, id: Player.Id, uid: Player.UserId): Player = {
+      val (whiteUid, blackUid) =
+        (uids.headOption.filter(_.nonEmpty), uids.lift(1).filter(_.nonEmpty))
+      def makePlayer(
+          field: String,
+          color: Color,
+          id: Player.Id,
+          uid: Player.UserId
+      ): Player = {
         val builder = r.getO[Player.Builder](field)(playerBSONHandler) | emptyPlayerBuilder
         val win = winC map (_ == color)
         builder(color)(id)(uid)(win)
@@ -104,13 +129,16 @@ object BSONHandlers {
               unmovedRooks = decoded.unmovedRooks,
               checkCount = if (gameVariant.threeCheck) {
                 val counts = r.intsD(F.checkCount)
-                CheckCount(counts.headOption getOrElse 0, counts.lastOption getOrElse 0)
-              }
-              else Game.emptyCheckCount
+                CheckCount(
+                  counts.headOption getOrElse 0,
+                  counts.lastOption getOrElse 0
+                )
+              } else Game.emptyCheckCount
             ),
             variant = gameVariant,
             crazyData =
-              if (gameVariant.crazyhouse) Some(r.get[Crazyhouse.Data](F.crazyData))
+              if (gameVariant.crazyhouse)
+                Some(r.get[Crazyhouse.Data](F.crazyData))
               else None
           ),
           color = turnColor
@@ -135,7 +163,12 @@ object BSONHandlers {
           clk <- chessGame.clock
           bw <- r bytesO F.whiteClockHistory
           bb <- r bytesO F.blackClockHistory
-          history <- BinaryFormat.clockHistory.read(clk.limit, bw, bb, if (status == Status.Outoftime) Some(turnColor) else None)
+          history <- BinaryFormat.clockHistory.read(
+            clk.limit,
+            bw,
+            bb,
+            if (status == Status.Outoftime) Some(turnColor) else None
+          )
         } yield history,
         mode = Mode(r boolD F.rated),
         createdAt = createdAt,
@@ -153,28 +186,51 @@ object BSONHandlers {
   }
 
   import chess.format.FEN
-  implicit val gameWithInitialFenBSONHandler: BSON[Game.WithInitialFen] = new BSON[Game.WithInitialFen] {
-    def reads(r: BSON.Reader): Game.WithInitialFen = {
-      Game.WithInitialFen(gameBSONHandler.reads(r), (r strO Game.BSONFields.initialFen).map(FEN.apply))
+  implicit val gameWithInitialFenBSONHandler: BSON[Game.WithInitialFen] =
+    new BSON[Game.WithInitialFen] {
+      def reads(r: BSON.Reader): Game.WithInitialFen = {
+        Game.WithInitialFen(
+          gameBSONHandler.reads(r),
+          (r strO Game.BSONFields.initialFen).map(FEN.apply)
+        )
+      }
+
+      def writes(w: BSON.Writer, o: Game.WithInitialFen) = ???
     }
 
-    def writes(w: BSON.Writer, o: Game.WithInitialFen) = ???
-  }
-
-  private def clockHistory(color: Color, clockHistory: Option[ClockHistory], clock: Option[Clock], flagged: Option[Color]) =
+  private def clockHistory(
+      color: Color,
+      clockHistory: Option[ClockHistory],
+      clock: Option[Clock],
+      flagged: Option[Color]
+  ) =
     for {
       clk <- clock
       history <- clockHistory
       times = history(color)
-    } yield BinaryFormat.clockHistory.writeSide(clk.limit, times, flagged contains color)
-
-  private[game] def clockBSONReader(since: DateTime, whiteBerserk: Boolean, blackBerserk: Boolean) = new BSONReader[BSONBinary, Color => Clock] {
-    def read(bin: BSONBinary) = BinaryFormat.clock(since).read(
-      ByteArrayBSONHandler read bin, whiteBerserk, blackBerserk
+    } yield BinaryFormat.clockHistory.writeSide(
+      clk.limit,
+      times,
+      flagged contains color
     )
+
+  private[game] def clockBSONReader(
+      since: DateTime,
+      whiteBerserk: Boolean,
+      blackBerserk: Boolean
+  ) = new BSONReader[BSONBinary, Color => Clock] {
+    def read(bin: BSONBinary) =
+      BinaryFormat
+        .clock(since)
+        .read(
+          ByteArrayBSONHandler read bin,
+          whiteBerserk,
+          blackBerserk
+        )
   }
 
-  private[game] def clockBSONWrite(since: DateTime, clock: Clock) = ByteArrayBSONHandler write {
-    BinaryFormat clock since write clock
-  }
+  private[game] def clockBSONWrite(since: DateTime, clock: Clock) =
+    ByteArrayBSONHandler write {
+      BinaryFormat clock since write clock
+    }
 }
